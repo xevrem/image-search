225b6c6b35277040a94692290c4c0e4e21854a59
erika jonell <erika.jonell@gmail.com>
2017-10-27 15:06:11 -0400

    updated api usage details

--- server.js
+++ server.js
@@ -9,6 +9,41 @@ let secret = process.env.IMGUR_CLIENT_ID;
 
 let searches = [];
 
+app.get('/', (req, res) => {
+    res.end(`
+USAGE:
+    IMAGE SEARCH:
+        https://xev-img-search.glitch.me/api/imagesearch/<query>
+
+        QUERY PARAMS:
+            <query> - what you are searching for (i.e., LOL-cats)
+
+        OPTIONAL PARAMS:
+            offset=<int> where <int> is the page to offset to
+
+    RECENT SEARCHES:
+        https://xev-img-search.glitch.me/api/latest/imagesearch
+
+RESULT EXAMPLES:
+    IMAGE SEARCH:
+        [0:
+            {
+                url:	"https://i.imgur.com/nnZL8tk.gif",
+                snippet:	"TRY NOT TO LAUGH or GRIN: Funny Monkeys VS Dogs and Cats Compilation",
+                thumbnail:	"https://i.imgur.com/nnZL8tk.gif",
+                context:	"https://i.imgur.com/nnZL8tk.gif"
+            }
+        ]
+    RECENT SEARCHES:
+        [0:
+            {
+                term:	"cats and dogs",
+                when:	"Fri Oct 27 2017 18:53:25 GMT+0000 (UTC)"
+            }
+        ]
+    `);
+})
+
 //perform the image search
 app.get('/api/imagesearch/:query', (req, res) => {
     //store search result
